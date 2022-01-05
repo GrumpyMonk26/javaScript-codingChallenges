3ec7712d2b95a66c13ef0374e9ddc84cd3169cdc
Jason Evans <evans.jason@hotmail.co.uk>
2022-01-05 14:37:07 +0000

    managing player state

--- PigGame/script.js
+++ PigGame/script.js
@@ -20,6 +20,7 @@ const scores = [0, 0];
 
 let currentScore = 0;
 let activePlayer = 0;
+let playing = true;
 
 const switchPlayer = function() {
     document.getElementById(`current--${activePlayer}`).textContent = 0;
@@ -31,33 +32,38 @@ const switchPlayer = function() {
 
 // roll button
 btnRoll.addEventListener('click', function(){
-    const dice = Math.trunc(Math.random() * 6) + 1;
-    console.log(dice)
-    diceEl.classList.remove('hidden');
-    diceEl.src = `dice-${dice}.png`
+    if(playing){
+        const dice = Math.trunc(Math.random() * 6) + 1;
+        console.log(dice)
+        diceEl.classList.remove('hidden');
+        diceEl.src = `dice-${dice}.png`
 
-    if( dice !== 1 ){
-        currentScore += dice;
-        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
-    } else {
-         switchPlayer();
+        if( dice !== 1 ){
+            currentScore += dice;
+            document.getElementById(`current--${activePlayer}`).textContent = currentScore;
+        } else {
+            switchPlayer();
+        }
     }
 })
 
 btnhold.addEventListener('click', function(){
+    if(playing){
     //Add current score to active palyer
-    scores[activePlayer] += currentScore;
+        scores[activePlayer] += currentScore;
 
-    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+        document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
 
-    if(scores[activePlayer] >= 100) {
-        document
-        .querySelector(`.player--${activePlayer}`)
-        .classList.add('player--winner')
-        document.
-        querySelector(`.player--${activePlayer}`)
-        .classList.remove('player--active')
-    }else{
-        switchPlayer();
+        if(scores[activePlayer] >= 100) {
+            playing = false;
+            document
+            .querySelector(`.player--${activePlayer}`)
+            .classList.add('player--winner')
+            document.
+            querySelector(`.player--${activePlayer}`)
+            .classList.remove('player--active')
+        }else{
+            switchPlayer();
+        }
     }
 })

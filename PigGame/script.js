7512001d67e5e62a594270116979ba2af20d5822
Jason Evans <evans.jason@hotmail.co.uk>
2022-01-05 14:22:53 +0000

    holding the current score

--- PigGame/script.js
+++ PigGame/script.js
@@ -21,6 +21,14 @@ const scores = [0, 0];
 let currentScore = 0;
 let activePlayer = 0;
 
+const switchPlayer = function() {
+    document.getElementById(`current--${activePlayer}`).textContent = 0;
+    currentScore = 0;
+    activePlayer = activePlayer === 0 ? 1: 0
+    player0El.classList.toggle('player--active')
+    player1El.classList.toggle('player--active')  
+}
+
 // roll button
 btnRoll.addEventListener('click', function(){
     const dice = Math.trunc(Math.random() * 6) + 1;
@@ -32,11 +40,24 @@ btnRoll.addEventListener('click', function(){
         currentScore += dice;
         document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
-        document.getElementById(`current--${activePlayer}`).textContent = 0;
-        currentScore = 0;
-        activePlayer = activePlayer === 0 ? 1: 0
-        player0El.classList.toggle('player--active')
-        player1El.classList.toggle('player--active')   
+         switchPlayer();
     }
 })
 
+btnhold.addEventListener('click', function(){
+    //Add current score to active palyer
+    scores[activePlayer] += currentScore;
+
+    document.getElementById(`score--${activePlayer}`).textContent = scores[activePlayer];
+
+    if(scores[activePlayer] >= 100) {
+        document
+        .querySelector(`.player--${activePlayer}`)
+        .classList.add('player--winner')
+        document.
+        querySelector(`.player--${activePlayer}`)
+        .classList.remove('player--active')
+    }else{
+        switchPlayer();
+    }
+})

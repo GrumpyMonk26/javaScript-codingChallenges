00eb54f6f66f76bc022a588203089402452060b1
Jason Evans <evans.jason@hotmail.co.uk>
2022-01-05 13:31:27 +0000

    switching the active player

--- PigGame/script.js
+++ PigGame/script.js
@@ -1,5 +1,7 @@
 'use strict';
 // selecting elements
+const player0El = document.querySelector('.player--0');
+const player1El = document.querySelector('.player--1');
 const score0El = document.querySelector('#score--0');
 const score1El = document.querySelector('#score--1');
 const diceEl = document.querySelector('.dice');
@@ -14,7 +16,10 @@ score0El.textContent = 0;
 score1El.textContent = 0;
 diceEl.classList.add('hidden');
 
-let currentScore = 0
+const scores = [0, 0];
+
+let currentScore = 0;
+let activePlayer = 0;
 
 // roll button
 btnRoll.addEventListener('click', function(){
@@ -25,9 +30,13 @@ btnRoll.addEventListener('click', function(){
 
     if( dice !== 1 ){
         currentScore += dice;
-        current0El.textContent = currentScore
+        document.getElementById(`current--${activePlayer}`).textContent = currentScore;
     } else {
-    
+        document.getElementById(`current--${activePlayer}`).textContent = 0;
+        currentScore = 0;
+        activePlayer = activePlayer === 0 ? 1: 0
+        player0El.classList.toggle('player--active')
+        player1El.classList.toggle('player--active')   
     }
 })
 

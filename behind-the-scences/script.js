c4a656edadd47665f429834c8cb4dedcbf39c7ba
Jason Evans <evans.jason@hotmail.co.uk>
2022-01-06 14:05:10 +0000

    Hoisting examples completed

--- behind-the-scences/script.js
+++ behind-the-scences/script.js
@@ -24,3 +24,36 @@ calcAge(1973);
 
 // HOISTING AND TZD IN PRACTICE
 
+// console.log(me);
+// console.log(job);
+// console.log(year);
+
+var me = 'Jason'
+let job = 'Web Developer'
+const year = 1973
+
+//functions
+console.log(addDecl(2, 3));
+// console.log(addExpr(2, 3));
+// console.log(addArrow(2, 3));
+
+function addDecl(a, b) {
+    return a + b
+}
+
+const addExpr = function(a, b) {
+    return a + b
+}
+
+const addArrow = (a, b) => a + b;
+
+if(!numberProducts){
+    deleteShoppingCart()
+}
+
+var numberProducts = 10;
+function deleteShoppingCart(){
+    console.log('All products deleted!')
+}
+
+// THIS KEYWORD EXAMPLES
